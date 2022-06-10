5caf8c6e15a191ac5577103d6fb3dd1729ccbafd
Chris Walker <thatamazingweb@gmail.com>
2022-06-10 21:44:18 +0100

    Remove k8s info from prompt and replace with vanilla fish function

--- dot_config/fish/functions/kk.fish
+++ dot_config/fish/functions/kk.fish
@@ -1,11 +1,8 @@
-function k8s_prompt \
+function kk \
   --description 'Output k8s cluster/namespace'
-
-  set -l k8s_prompt
 
   set k8s_ctx (kubectl config current-context)
   set k8s_ns (kubectl config view -o=jsonpath="{.contexts[?(@.name==\"$k8s_ctx\")].context.namespace}")
-  set k8s_prompt "$bold_green$k8s_ctx/$k8s_ns"
 
-  echo $k8s_prompt
+  printf "%s/%s" $k8s_ctx $k8s_ns
 end

e6bda584644d33bf393d3622af10988b8ad0c80e
Chris Walker <thatamazingweb@gmail.com>
2024-08-04 20:09:51 +0100

    Tweaks to ansi term theming following switch to Plan 9 theme colours

--- dot_config/fish/ansi_term.fish
+++ dot_config/fish/ansi_term.fish
@@ -5,25 +5,25 @@
 # @author Chris Walker
 #
 
-set fish_color_error brred --underline
-set fish_color_comment brblack
+set fish_color_error red --underline
+set fish_color_comment black
 set fish_color_quote green
 set fish_color_match bryellow
-set fish_color_command brblue
-set fish_color_param brwhite
-set fish_color_autosuggestion brblack
+set fish_color_command black
+set fish_color_param blue
+set fish_color_autosuggestion blue
 set fish_color_redirection magenta
 set fish_color_end white
 set fish_color_operator magenta
 set fish_color_escape magenta
 
 set fish_pager_color_description blue
-set fish_pager_color_prefix brwhite --bold
+set fish_pager_color_prefix black --bold
 set fish_pager_color_completion blue
 
-set fish_pager_color_selected_description black
-set fish_pager_color_selected_prefix white --bold
-set fish_pager_color_selected_completion white --bold
+set fish_pager_color_selected_description brwhite --bold
+set fish_pager_color_selected_prefix brcyan --bold
+set fish_pager_color_selected_completion brwhite --bold
 set fish_pager_color_selected_background --background=blue
 
 set fish_pager_color_progress bryellow --bold

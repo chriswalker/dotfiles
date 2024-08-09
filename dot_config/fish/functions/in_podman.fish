71046a810704daa4784409ab4a50885ba1c260f7
Chris Walker <thatamazingweb@gmail.com>
2024-08-09 13:35:55 +0100

    Remove toolbox/podman/work templating from fish config, and associated fish functions

--- dot_config/fish/functions/in_podman.fish
+++
@@ -1,8 +0,0 @@
-function in_podman --description 'Determine whether fish shell is running in a Podman container'
-  # Assumes Podman currently 
-  if test -e /run/.containerenv
-      echo "true"
-      return
-  end
-  echo "false"
-end
--- dot_config/fish/functions/toolbox_name.fish
+++
@@ -1,8 +0,0 @@
-function toolbox_name --description 'Return the name of the toolbox'
-  # Assumes Podman/Toolbox currently 
-  if string match -q "true" (in_podman)
-      echo (grep name /run/.containerenv | cut -d '"' -f2)
-      return
-  end
-  echo ""
-end
